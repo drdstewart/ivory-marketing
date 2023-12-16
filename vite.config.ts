3796116263cc13a874e0381872328cdb88053fc7
drdstewart <48361766+drdstewart@users.noreply.github.com>
2023-12-16 18:57:04 +0000

    remove old settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: "/src/main.js",
-    },
-  },
   plugins: [react()],
+  base: "/",
 });
