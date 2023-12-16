5f4cb2703b31a78d23b0010db5d4988417e06c5e
drdstewart <48361766+drdstewart@users.noreply.github.com>
2023-12-16 11:23:24 +0000

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "/src/main.js",
+    },
+  },
   plugins: [react()],
-})
+});
